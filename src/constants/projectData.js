1e894dfc5ba90c00ae9352c46ec470dfe53f469f
purpledmango <nitishmerlin@gmail.com>
2024-03-07 15:53:37 +0530

    Resume update

--- src/constants/projectData.js
+++ src/constants/projectData.js
@@ -1,14 +1,22 @@
 const projectData = [
   {
     name: "The Silk Store - Ecommerce Frontend",
-  
+
     repoLink: "https://github.com/purpledmango/Silk-store-Reactjs-ecom",
-    img:"https://i.imgur.com/N4hhjDm.png",
+    img: "https://i.imgur.com/N4hhjDm.png",
     description:
       "I developed an open-source project named 'The Silk Store' with a ReactJs-powered front-end and an integrated back-end for both administrators and customers.",
   },
-  
-  
+  {
+    name: "Blog Manager Dashboard - Fullstack",
+
+    repoLink: "https://github.com/purpledmango/Silk-store-Reactjs-ecom",
+    img: "https://i.imgur.com/N4hhjDm.png",
+    description:
+      "I developed an open-source project named 'The Silk Store' with a ReactJs-powered front-end and an integrated back-end for both administrators and customers.",
+  }
+
+
 ];
 
 export default projectData;
